069cccde43a35f0ff7e474924a87e6a1a1d50379
Robin John Hopkins <robinjohnhopkins@Robins-MacBook-Pro.local>
2018-05-26 10:26:06 +0100

    production var missing

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -6,12 +6,12 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-  mongoUser = process.env[mongoServiceName + '_USER'];
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+  var mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
+  var mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
+  var mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
+  var mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
+  var mongoUser = process.env[mongoServiceName + '_USER'];
   console.log(mongoServiceName, mongoHost, mongoPort, mongoDatabase,
     mongoPassword, mongoUser);
 
