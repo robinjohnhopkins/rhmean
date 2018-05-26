b23ac6e0551807359d93873e222df1e69b9fb9c1
Robin John Hopkins <robinjohnhopkins@Robins-MacBook-Pro.local>
2018-05-26 10:18:25 +0100

    add openshift db vars

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,5 +1,47 @@
 'use strict';
 /*eslint no-process-env:0*/
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+    mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+    mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+  mongoUser = process.env[mongoServiceName + '_USER'];
+  console.log(mongoServiceName, mongoHost, mongoPort, mongoDatabase,
+    mongoPassword, mongoUser);
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+
+  }
+}
+if (mongoURL == null) {
+  // mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+  mongoURL = 'mongodb://mongo:mongo@localhost:27017/exampledb?authSource=admin';
+}
+var openshiftVars = {
+  port: port,
+  ip: ip,
+  mongoURLLabel: mongoURLLabel,
+  mongoURL: mongoURL,
+  once: null
+};
+if (openshiftVars.once ===null){
+  console.log('openshiftVars are ========================');
+  console.log(openshiftVars);
+  openshiftVars.once = 'done_once';
+}
 
 // Production specific configuration
 // =================================
@@ -19,6 +61,7 @@ module.exports = {
     uri: process.env.MONGODB_URI
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
+      || mongoURLLabel
       || 'mongodb://localhost/mean1'
   }
 };
